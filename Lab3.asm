9724ae9bde65d1e6f63dff78fc3b11dcb6e177c8
pavlohaidin <pasha.gajdin@gmail.com>
2025-12-15 21:20:28 +0000

    asembler code

---
+++ Lab3.asm
@@ -0,0 +1,15 @@
+global _start
+section .text
+_start:
+; Підготовка до системного виклику write
+mov rax, 1 ; Номер системного виклику write
+mov rdi, 1 ; Файловий дескриптор (1 = stdout)
+mov rsi, message ; Адреса повідомлення
+mov rdx, 7 ; Довжина повідомлення
+syscall ; Виклик системного виклику
+; Підготовка до системного виклику exit
+mov rax, 60 ; Номер системного виклику exit
+xor rdi, rdi ; Статус виходу 0
+syscall ; Виклик системного виклику 32
+section .data
+message: db "KI-241", 10 ; 10 - це символ нового рядка
